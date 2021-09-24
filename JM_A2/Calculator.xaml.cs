af3ca7ad10aa2328e2504d1f2cd84c5ba9eaedb4
Jovanka <atlant81@yahoo.com>
2021-09-24 12:14:36 -0400

    First commit

---
+++ JM_A2/Calculator.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace JM_A2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Calculator : Page
+    {
+        string input = "";
+        string strNum1 = "";
+        string strNum2 = "";
+        char operation;
+        int result = 0;
+
+        public Calculator()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            tb_result.Text = "";
+            input += "7";
+            tb_result.Text += input;
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+            tb_result.Text = "";
+            input += "8";
+            tb_result.Text += input;
+        }
+
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {
+            tb_result.Text = "";
+            input += "9";
+            tb_result.Text += input;
+        }
+
+        private void ButtonDiv_Click(object sender, RoutedEventArgs e)
+        {
+            strNum1 = input;
+            operation = '/';
+            input = "";
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            tb_result.Text = "";
+            input += "4";
+            tb_result.Text += input;
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            tb_result.Text = "";
+            input += "5";
+            tb_result.Text += input;
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            tb_result.Text = "";
+            input += "6";
+            tb_result.Text += input;
+        }
+
+        private void ButtonPlus_Click(object sender, RoutedEventArgs e)
+        {
+            strNum1 = input;
+            operation = '+';
+            input = "";
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            tb_result.Text = "";
+            input += "1";
+            tb_result.Text += input;
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            tb_result.Text = "";
+            input += "2";
+            tb_result.Text += input;
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            tb_result.Text = "";
+            input += "3";
+            tb_result.Text += input;
+        }
+
+        private void ButtonMin_Click(object sender, RoutedEventArgs e)
+        {
+            strNum1 = input;
+            operation = '-';
+            input = "";
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            input = "";
+            strNum1 = "";
+            strNum2 = "";
+            result = 0;
+            tb_result.Text = "";
+        }
+
+        private void Button0_Click(object sender, RoutedEventArgs e)
+        {
+            tb_result.Text = "";
+            input += "0";
+            tb_result.Text += input;
+        }
+
+        private void ButtonMul_Click(object sender, RoutedEventArgs e)
+        {
+            strNum1 = input;
+            operation = '*';
+            input = "";
+        }
+
+        private void ButtonEq_Click(object sender, RoutedEventArgs e)
+        {
+            strNum2 = input;
+            int num1;
+            int num2;
+
+            Int32.TryParse(strNum1, out num1);
+            Int32.TryParse(strNum2, out num2);
+
+            switch (operation)
+            {
+                case '+':
+                    {
+                        result = num1 + num2;
+                        tb_result.Text = result.ToString();
+                        break;
+                    }
+                case '-':
+                    {
+                        result = num1 - num2;
+                        tb_result.Text = result.ToString();
+                        break;
+                    }
+                case '*':
+                    {
+                        result = num1 * num2;
+                        tb_result.Text = result.ToString();
+                        break;
+                    }
+                case '/':
+                    {
+                        if (num2 != 0)
+                        {
+                            result = num1 / num2;
+                            tb_result.Text = result.ToString();
+                            break;
+                        }
+                        else
+                        {
+                            tb_result.Text = "Cannot divide by 0";
+                            break;
+                        }
+
+                    }
+                default:
+                    break;
+            }
+            input = "";
+            operation = '\0';
+        }
+    }
+}
---
+++ JM_A2/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace JM_A2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.Loaded += MainPage_Loaded;
+            this.mainFrame.Navigated += MainFrame_Navigated;
+
+
+            SystemNavigationManager.GetForCurrentView().BackRequested += OnBackRequested;
+        }
+
+        private void OnBackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (mainFrame.CanGoBack)
+            {
+                e.Handled = true;
+                mainFrame.GoBack();
+            }
+        }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            btn_back.IsEnabled = mainFrame.CanGoBack;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(typeof(Home));
+        }
+
+        private void OnGoHomeClick(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(typeof(Home));
+        }
+
+        private void OnGoCalculatorClick(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(typeof(Calculator));
+        }
+
+        private void OnGoAboutClick(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(typeof(Developer));
+        }
+
+        private void OnGoBackClick(object sender, RoutedEventArgs e)
+        {
+            if (mainFrame.CanGoBack) mainFrame.GoBack();
+        }
+
+        private void OpenSplitViewClick(object sender, RoutedEventArgs e)
+        {
+            this.mainSplitView.IsPaneOpen = !this.mainSplitView.IsPaneOpen;
+        }
+    }
+}
